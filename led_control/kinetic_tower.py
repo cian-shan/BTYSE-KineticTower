a2f4025b09904e67e17c02d72594caea9c3b56f7
Enda Kilgarriff <enda.kilgarriff@analog.com>
2022-12-11 18:07:41 +0000

    fixed player references, tidy

--- led_control/kinetic_tower.py
+++ led_control/kinetic_tower.py
@@ -17,6 +17,7 @@ from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
 import subprocess
 from csv import writer
 import os 
+from threading import Thread
 
 STANDBY = 0
 COUNTDOWN = 1
@@ -25,7 +26,7 @@ RESULTS = 3
 FULL_BRIGHTNESS = 1
 LED_COUNT = 1085
 LED_HEIGHT = 180
-GAME_WIN_LEVEL = 9
+GAME_WIN_LEVEL = 60
 
 LINE_UP = u"\u001b[1A"
 LINE_CLEAR = u"\u001b[1K"
@@ -75,9 +76,9 @@ class KineticTowerGame:
             Blink(self.winner.pixel_map, speed=0.3, color=color.GREEN),
             Solid(self.not_winner.pixel_map, color.BLACK),
         )
-        print("Game over!")
+        print("\nGame over!")
         print(GREEN, "Winner :", self.winner.player_ID)
-        print(GREEN, "Score: ", game.game_duration)
+        print(GREEN, f"Score  : {self.game_duration:8.2f} s")
         for i in range(5):
             result_leds.animate()
             time.sleep(1)
@@ -91,6 +92,11 @@ class KineticTowerGame:
             interations.writerow(interaction)
             file.close()
 
+    def get_game_go_from_console():
+        game_go = input()
+        return game_go
+
+
 
 if __name__ == "__main__":
     print("Kinetic Tower Starting")
@@ -98,8 +104,8 @@ if __name__ == "__main__":
     ## Setup Power Sensors
     i2c_bus = board.I2C()
 
-    p1_ina219 = INA219(i2c_bus, addr=0x40)
-    p2_ina219 = INA219(i2c_bus, addr=0x44)
+    p2_ina219 = INA219(i2c_bus, addr=0x40)
+    p1_ina219 = INA219(i2c_bus, addr=0x44)
 
     p1_ina219.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
     p1_ina219.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
@@ -110,8 +116,8 @@ if __name__ == "__main__":
     p2_ina219.bus_voltage_range = BusVoltageRange.RANGE_16V
 
     # Create Strip Objects
-    p2 = Player(player_ID=1, input_pin=24, power_sensor=p1_ina219)
-    p1 = Player(player_ID=2, input_pin=25, power_sensor=p2_ina219)
+    p2 = Player(player_ID='PLAYER 2', input_pin=24, power_sensor=p2_ina219)
+    p1 = Player(player_ID='PLAYER 1', input_pin=25, power_sensor=p1_ina219)
 
     game = KineticTowerGame()
 
@@ -189,7 +195,9 @@ if __name__ == "__main__":
             clear_leds.animate()
             # Set players back to start
             os.system('clear')
+            print(RESET)
             print("Entering Standby")
+            print("Who can generate the power needed in the stortest time?!")
             while game.game_status == STANDBY:
                 standby_leds.animate()
                 # test_leds.animate()
@@ -202,17 +210,17 @@ if __name__ == "__main__":
             # Set all LEDs to Countdown
             print("Countdown!")
             game.game_status = IN_GAME
-            for i in range(4):
+            for i in range(0,3):
                 countdown_leds.activate(i)
                 countdown_leds.animate()
-                time.sleep(1)
-                print(i)
+                print(4-(i+1))
+                time.sleep(.75)
             countdown_leds.freeze()
             pass
             pass
 
         elif game.game_status == IN_GAME:
-            print("In Game")
+            print("GO!!!")
             KineticTowerGame.log_interaction()
             clear_leds.animate()
             game_start_time = time.time()
@@ -226,28 +234,33 @@ if __name__ == "__main__":
                 game_time = time.time() - game_start_time 
 
                 # Print Progress
-                print(f'Player 1: {p1.player_leds.level=:.2f} || Player 2: {p2.player_leds.level=:.2f}\n', end = '')
-                print(f'Game Score: {game_time=:.2f}', end='')
+                print(f'Player 1: {p1.player_leds.level:8.2f}\nPlayer 2: {p2.player_leds.level:8.2f}\n', end = '')
+                print(f'Game Score: {game_time:8.2f}', end='')
 
-                print(LINE_UP, LINE_UP, LINE_CLEAR)
+                print(LINE_UP, LINE_UP, LINE_UP, LINE_CLEAR)
+                
 
                 # check for winner
                 # Somewhere the player have gotten mixed up, this will print the correct winner however
-                if p2.energy_gen > GAME_WIN_LEVEL: 
+                if p1.energy_gen > GAME_WIN_LEVEL:
+                    print("PLAYER 1 WIN") 
+                    print("\n\n\n")
+                    game.winner = p1
+                    game.not_winner = p2
+                    game.tot_energy = p1.energy_gen + p2.energy_gen
+                    game.game_status = RESULTS
+                    game.game_duration = game_time
+                    #print(LINE_UP, LINE_UP, LINE_CLEAR)
+
+                elif p2.energy_gen > GAME_WIN_LEVEL:
+                    print("PLAYER 2 WIN")
+                    print("\n\n\n")
                     game.winner = p2
                     game.not_winner = p1
                     game.tot_energy = p1.energy_gen + p2.energy_gen
                     game.game_status = RESULTS
                     game.game_duration = game_time
-                    print(LINE_UP, LINE_UP, LINE_CLEAR)
-
-                elif p1.energy_gen > GAME_WIN_LEVEL:
-                    game.winner = p1
-                    game.not_winner = p2
-                    game.tot_energy = p1.energy_gen + p2.energy_gen
-                    game.game_status = RESULTS
-                    game.duration = game_time
-                    print(LINE_UP, LINE_UP, LINE_CLEAR)
+                    #print(LINE_UP, LINE_UP, LINE_CLEAR)
                 
                 pass
         elif game.game_status == RESULTS:
