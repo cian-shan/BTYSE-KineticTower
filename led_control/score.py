6e0e8f1034dc2f843c10c219b775111ff3e16743
ekilgarr <Enda.Kilgarriff@analog.com>
2022-12-20 22:02:35 +0000

    Working on leaderboard

--- led_control/score.py
+++ led_control/score.py
@@ -3,8 +3,11 @@ import time
 from dicttoxml import dicttoxml
 from xml.dom.minidom import parseString
 import os
+import xmltodict
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '169.254.207.119'
+PORT = 9000
+CLIENT_IP = socket.gethostbyname(socket.gethostname())
 
 
 class Score():
@@ -14,18 +17,66 @@ class Score():
         self.entry_name =  entry_name
         self.school_name = school_name
         self.score = score
+        self.message_id = id(self)
+        self.receiver_ip = str(TCP_IP)
+        self.sender_ip = str(CLIENT_IP)
 
     def create_xml(self):
-        xml = dicttoxml(self, attr_type=False, custom_root='Score')
-        #print(xml)
+        # xml = dicttoxml(self, attr_type=True, custom_root='Score')
+        # print(xml)
 
-        dom = parseString(xml)
+        # dom = parseString(xml)
         # print(dom.toprettyxml())
+
+        score = {
+            "NewScoreMessage":
+            {
+                '@GameId': self.game_name,
+                '@MessageID': (str(self.message_id)),
+                '@Type' : "NewScoreMessage",
+                '@Receiver' : self.receiver_ip,
+                '@Sender': self.sender_ip,
+                '@xmlns:xsd' : "http://www.w3.org/2001/XMLSchema",
+                '@xmlns:xsi' : "http://www.w3.org/2001/XMLSchema-instance",
+                "NewScore" : str(self.score)
+            }
+        }
+
+        xml = xmltodict.unparse(score, pretty=True, encoding='UTF-16')
+
+        s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        s1.connect(TCP_IP)
+
+        s1.send(xml)
+
+        print(xml)
 
     def send_score():
         pass
 
-    def get_top_10():
+    def get_top_10(self):
+        score = {
+            "RequestScoreMessage":
+            {
+                '@GameId': self.game_name,
+                '@MessageID': (str(self.message_id)),
+                '@Type' : "RequestScoreMessage",
+                '@Receiver' : self.receiver_ip,
+                '@Sender': self.sender_ip,
+                '@xmlns:xsd' : "http://www.w3.org/2001/XMLSchema",
+                '@xmlns:xsi' : "http://www.w3.org/2001/XMLSchema-instance",
+            }
+        }
+
+        xml = xmltodict.unparse(score, pretty=True, encoding='UTF-16')
+
+        s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        s1.connect((TCP_IP, PORT))
+
+        s1.send(xml)  
+
         pass
 
     def print_gameplay(self):
@@ -35,12 +86,16 @@ class Score():
 
 if __name__ == "__main__":
 
-    
-
     # Test creating an XML Score 
     # score1 = vars(Score("Jenny", "School NS", 120))
     # Score.create_xml(score1)
-    # Score.log_interaction()
+
+    s1 = Score("Jenny", "School NS", 120)
+    # Score.create_xml(s1)
+
+    #Score.log_interaction()
+
+    """
 
     LINE_UP = u"\u001b[1A"
     LINE_CLEAR = u"\u001b[1K"
@@ -82,7 +137,7 @@ if __name__ == "__main__":
 
         time.sleep(.2)
 
-    
+    """
     
 
 
