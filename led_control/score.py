90da70855c96681889da8a102ca48991721662bb
ekilgarr <Enda.Kilgarriff@analog.com>
2022-12-22 11:31:31 +0000

    Add test server and scores

--- led_control/score.py
+++ led_control/score.py
@@ -1,3 +1,4 @@
+
 import socket 
 import time
 from dicttoxml import dicttoxml
@@ -5,15 +6,16 @@ from xml.dom.minidom import parseString
 import os
 import xmltodict
 
-TCP_IP = '169.254.207.119'
+TCP_IP = "127.0.0.1"
 PORT = 9000
-CLIENT_IP = socket.gethostbyname(socket.gethostname())
-
+HOSTNAME = socket.gethostname()
+CLIENT_IP = socket.gethostbyname(HOSTNAME)
+GAME_NAME = "Kinetic Tower"
 
 class Score():
 
-    def __init__(self, entry_name, school_name, score):
-        self.game_name = "kinetic_tower"
+    def __init__(self, entry_name=None, school_name=None, score=None):
+        self.game_name = GAME_NAME
         self.entry_name =  entry_name
         self.school_name = school_name
         self.score = score
@@ -46,11 +48,12 @@ class Score():
 
         s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        s1.connect(TCP_IP)
+        s1.connect((TCP_IP, PORT))
 
-        s1.send(xml)
+        s1.send(xml.encode())
+        # data = s1.recv(1024)
 
-        print(xml)
+        # print("data : ", data)
 
     def send_score():
         pass
@@ -60,10 +63,10 @@ class Score():
             "RequestScoreMessage":
             {
                 '@GameId': self.game_name,
-                '@MessageID': (str(self.message_id)),
+                '@MessageID': (self.message_id),
                 '@Type' : "RequestScoreMessage",
-                '@Receiver' : self.receiver_ip,
-                '@Sender': self.sender_ip,
+                '@Receiver' : TCP_IP,
+                '@Sender': CLIENT_IP,
                 '@xmlns:xsd' : "http://www.w3.org/2001/XMLSchema",
                 '@xmlns:xsi' : "http://www.w3.org/2001/XMLSchema-instance",
             }
@@ -71,11 +74,18 @@ class Score():
 
         xml = xmltodict.unparse(score, pretty=True, encoding='UTF-16')
 
+        #print(xml)
+
         s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         s1.connect((TCP_IP, PORT))
 
-        s1.send(xml)  
+        s1.send(xml.encode())
+        #data = s1.recv(1024)
+
+        #print("Request top 10 : ", data)
+
+        # s1.send(xml)
 
         pass
 
@@ -86,12 +96,18 @@ class Score():
 
 if __name__ == "__main__":
 
+    print(CLIENT_IP)
+    print("HOSTNAME: ", HOSTNAME)
+
     # Test creating an XML Score 
     # score1 = vars(Score("Jenny", "School NS", 120))
     # Score.create_xml(score1)
-
     s1 = Score("Jenny", "School NS", 120)
-    # Score.create_xml(s1)
+    s2 = Score()
+    # Submit new score
+    s1.create_xml()
+    # Get to 10 for leaderboard
+    s2.get_top_10()
 
     #Score.log_interaction()
 
@@ -138,7 +154,3 @@ if __name__ == "__main__":
         time.sleep(.2)
 
     """
-    
-
-
-
