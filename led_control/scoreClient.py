283b3609258d94c319947148bdca27352288d5b8
Enda Kilgarriff <enda.kilgarriff@analog.com>
2023-11-26 20:15:21 +0000

    Clean up of scoreClient

--- led_control/scoreClient.py
+++ led_control/scoreClient.py
@@ -18,6 +18,9 @@ class ScoreClient:
         self.score_queue = []
 
     def submit_score(self, entry_name, school_name, score):
+        """
+            Create a new score from user data, submit it to server
+        """
 
         new_score = Score(score_client=self, entry_name=entry_name, school_name=school_name, score=score)
 
@@ -72,8 +75,12 @@ class ScoreClient:
 
             if int(rx_msg_id) == int(new_score.message_id):
                 print("Ack received from server")
+                return True
             else:
                 raise ConnectionError("No ack from server")
+                
+            
+
 
 
     def get_top_10(self):
@@ -97,11 +104,14 @@ class ScoreClient:
         # define separator between header and data
         # https://github.com/jchristn/WatsonTcp/blob/master/FRAMING.md
         separator = "\r\n\r\n"
+
         # Try to capture top 10 list
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(5)
                 s.connect((self.host_ip, self.port))
+
+                # Create request command
                 data = xml.encode()  # data to send
                 meta = {}  # any metadata to include
                 dic = {"len": len(data), "s": "Normal", "md": meta}
@@ -113,36 +123,35 @@ class ScoreClient:
                 print("Request for 10 Top sent")
                 # Expecting 1024 bytes back from server
                 
+                # Get header data from server
                 top_10_header = s.recv(1024)
                 header_xml = top_10_header.decode().strip()
                 clean_header = header_xml.replace("{\"s\":\"Normal\",\"len\":", "")
                 header_length = clean_header.replace("}", "")
-
                 print("Bytes to get in 10 TOP: ", int(header_length))
+
+                # Get data from server
                 top_10_bytes = s.recv(int(header_length))
                 print("Bytes Recieved in 1st req: ", len(top_10_bytes))
-                # Make sure all data is collected
+
+                # Make sure all data is collected - keep collecting data until expected amount has been retrived
                 while len(top_10_bytes) != int(header_length):
                     recv_more = s.recv(int(header_length))
                     top_10_bytes = top_10_bytes + recv_more
                 
-                if len(top_10_bytes) != int(header_length):
-                    s.close()
-                    raise Exception("Error Gathering Data - will try again")
-                else:
-                    top_10_xml = top_10_bytes.decode().strip()
-                    # print(f"top_10_xml\n\n {top_10_xml}")
-                    full_msg = xmltodict.parse(top_10_xml, encoding="UTF-16")
-                    # print("full_msg" + str(full_msg))
-                    list_of_scores = full_msg["TopScoreListMessage"]["TopScoresList"]["Score"]
-                    self.print_top10(list_of_scores)
-                    s.close()
-                    return list_of_scores
+                # Parse XML
+                top_10_xml = top_10_bytes.decode().strip()
+                full_msg = xmltodict.parse(top_10_xml, encoding="UTF-16")
+                list_of_scores = full_msg["TopScoreListMessage"]["TopScoresList"]["Score"]
+                self.print_top10(list_of_scores)
 
-            s.close()
+                s.close()
+                return list_of_scores
+
         # If not all data is collected try again until try limit is met
         except Exception as e:
-            attempt_limit = 0
+            # attempt_limit allows you to change how many retires are made
+            attempt_limit = 1
             if self.attempt_num < attempt_limit:
                 print(self.attempt_num)
                 time.sleep(1)
@@ -171,8 +180,9 @@ class ScoreClient:
 if __name__ == "__main__":
     demo_client = ScoreClient(game_name="Kinetic Tower", client_ip="192.168.7.40", host_ip="192.168.7.210")
 
-    # demo_client.submit_score(entry_name="Test", school_name="ADI", score=10)
-    for i in range(0, 10):
-        time.sleep(1)
-        demo_client.get_top_10()
+    # Submit a score
+    demo_client.submit_score(entry_name="Test", school_name="ADI", score=10)
+
+    # Get top 10
+    demo_client.get_top_10()
     
