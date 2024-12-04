a5b642834cd206958526d7ff52e26d3397deab27
cian-shan <cian.shanahan@hotmail.com>
2024-12-04 18:42:33 +0000

    faster LED updates

--- kinetic_tower/led_power_level.py
+++ kinetic_tower/led_power_level.py
@@ -1,20 +1,20 @@
 from adafruit_led_animation.animation import Animation
 from adafruit_led_animation.animation.blink import Blink
 import adafruit_led_animation.color as color
-
+import board
+import neopixel
 
 class PowerLevel(Animation):
     """
     This function is an abstraction of the Animation Class. 
     It controls the level of the LEDs
-
     """
     
     def __init__(self, pixel_object, color, max_height, level=0, name=None):
         self._num_pixels = len(pixel_object)
         self.max_height = max_height
         self.level = level
-        super().__init__(pixel_object, speed=1, color=color, name=name)
+        super().__init__(pixel_object, speed=0.05, color=color, name=name)  # Lower speed value for faster animation
 
     def update_level(self, new_level):
         self.level = new_level 
@@ -24,9 +24,8 @@ class PowerLevel(Animation):
         return super().reset()
 
     def draw(self):
-        # print("LEVEL: ", self.level)
         if self.level <= self.max_height:
-            fill_level = [self.color for i in range(self.level + 1)]
-            self.pixel_object[((self._num_pixels - 1) - self.level) :] = fill_level
+            fill_level = [self.color] * (self.level + 1)
+            self.pixel_object[-(self.level + 1):] = fill_level
         else:
-            self.pixel_object[((self._num_pixels - 1) - self.level) :] = self.max_height
+            self.pixel_object[-self.max_height:] = [self.color] * self.max_height
